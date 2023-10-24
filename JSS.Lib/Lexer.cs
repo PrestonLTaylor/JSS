fa0d44b6875a59f24de280ac81cb990086684111
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-10-24 20:38:36 +0100

    JSS.Lib: Clean up the lexer code

--- JSS.Lib/Lexer.cs
+++ JSS.Lib/Lexer.cs
@@ -2,28 +2,26 @@
 
 internal sealed class Lexer
 {
-	private readonly Consumer consumer;
+	private readonly Consumer _consumer;
 
 	public Lexer(string toLex)
 	{
-		consumer = new Consumer(toLex);
+		_consumer = new Consumer(toLex);
 	}
 
 	public IEnumerable<Token> Lex()
 	{
-		while (consumer.CanConsume())
+		while (_consumer.CanConsume())
 		{
-			// FIXME: Clean up after implemented
-			var current = consumer.Peek();
-			if (IsWhiteSpace(current))
+			if (IsWhiteSpace())
 			{
-				consumer.Consume();
+				IgnoreWhiteSpace();
 			}
-			else if (IsLineTerminator(current))
+			else if (IsLineTerminator())
 			{
-				yield return new Token { type = TokenType.LineTerminator, data = consumer.Consume().ToString() };	
+				yield return LexLineTerminator();
 			}
-			else if (IsComment(current))
+			else if (IsComment())
 			{
 				IgnoreComment();
 			}
@@ -37,30 +35,43 @@ internal sealed class Lexer
 	}
 
     // 12.2 White Space, https://tc39.es/ecma262/#sec-white-space
-    static private bool IsWhiteSpace(char codePoint)
+    private bool IsWhiteSpace()
 	{
 		const string whiteSpaceCodePoints = "\u0009\u000B\u000C\uFEFF\u0020\u00A0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000";
+
+		var codePoint = _consumer.Peek();
 		return whiteSpaceCodePoints.Contains(codePoint);
     }
 
+	private void IgnoreWhiteSpace()
+	{
+		_consumer.ConsumeWhile((_) => IsWhiteSpace());
+	}
+
     // 12.3 Line Terminators, https://tc39.es/ecma262/#sec-line-terminators
-    static private bool IsLineTerminator(char codePoint)
+    private bool IsLineTerminator()
 	{
         const string lineTerminatorCodePoints = "\u000A\u000D\u2028\u2029";
+
+		var codePoint = _consumer.Peek();
         return lineTerminatorCodePoints.Contains(codePoint);
     }
 
-    // 12.4 Comments, https://tc39.es/ecma262/#sec-comments
-	private bool IsComment(char firstCodePoint)
+	private Token LexLineTerminator()
 	{
-		var secondCodePoint = consumer.Peek(1);
-		return firstCodePoint == '/' && secondCodePoint == '/';
+		return new Token { type = TokenType.LineTerminator, data = _consumer.Consume().ToString() };
+	}
+
+    // 12.4 Comments, https://tc39.es/ecma262/#sec-comments
+	private bool IsComment()
+	{
+		return _consumer.Matches("//");
 	}
 
     // FIXME: Comments behave like white space and are discarded except that, if a MultiLineComment contains a line terminator code point,
 	// then the entire comment is considered to be a LineTerminator for purposes of parsing by the syntactic grammar
     private void IgnoreComment()
 	{
-		consumer.ConsumeWhile((codePoint) => !IsLineTerminator(codePoint));
+		_consumer.ConsumeWhile((_) => !IsLineTerminator());
 	}
 }
