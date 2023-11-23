74131be464f4dcb72d860e4f86372b020b3a7bf1
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-11-23 22:18:01 +0000

    JSS.Lib: Implement the ToPrimitive Abstract Operation

---
+++ JSS.Lib.UnitTests/AbstractOperationTests.cs
@@ -0,0 +1,36 @@
+﻿using JSS.Lib.AST.Value;
+using Boolean = JSS.Lib.AST.Value.Boolean;
+using String = JSS.Lib.AST.Value.String;
+
+namespace JSS.Lib.UnitTests;
+
+internal sealed class AbstractOperationTests
+{
+    static private readonly List<Value> toPrimitiveIdentityTestCases = new()
+    {
+        new Null(),
+        new Boolean { Value = false },
+        new Boolean { Value = true },
+        new Number { Value = 0.0 },
+        new Number { Value = 1.0 },
+        new Number { Value = 123456789.0 },
+        new Number { Value = 123456789.0 },
+        new String(""),
+        new String("'"),
+        new String("\""),
+        new String("This is a string literal"),
+    };
+
+    [TestCaseSource(nameof(toPrimitiveIdentityTestCases))]
+    public void ToPrimitive_ReturnsTheSameValue_WhenNotProvidingAnObject(Value expected)
+    {
+        // Arrange
+
+        // Act
+        var completion = expected.ToPrimitive();
+
+        // Assert
+        Assert.That(completion.IsNormalCompletion(), Is.True);
+        Assert.That(completion.Value, Is.SameAs(expected));
+    }
+}
--- JSS.Lib/AST/Value/Value.cs
+++ JSS.Lib/AST/Value/Value.cs
@@ -1,4 +1,6 @@
-﻿namespace JSS.Lib.AST.Value;
+﻿using JSS.Lib.Execution;
+
+namespace JSS.Lib.AST.Value;
 
 // FIXME: This is a very inefficient way of storing JS values.
 // 6.1 ECMAScript Language Types, https://tc39.es/ecma262/#sec-ecmascript-language-types
@@ -12,4 +14,27 @@ internal abstract class Value
     virtual public bool IsNumber() { return false; }
     virtual public bool IsBigInt() { return false; }
     virtual public bool IsObject() { return false; }
+
+    // 7.1.1 ToPrimitive ( input FIXME: [ , preferredType ] ), https://tc39.es/ecma262/#sec-toprimitive
+    public Completion ToPrimitive()
+    {
+        // FIXME: 1 .If input is an Object, then
+        // FIXME: a. Let exoticToPrim be ? GetMethod(input, @@toPrimitive).
+        // FIXME: b. If exoticToPrim is not undefined, then
+        // FIXME: i. If preferredType is not present, then
+        // FIXME: 1. Let hint be "default".
+        // FIXME: ii. Else if preferredType is STRING, then
+        // FIXME: 1. Let hint be "string".
+        // FIXME: iii. Else,
+        // FIXME: 1. Assert: preferredType is NUMBER.
+        // FIXME: 2. Let hint be "number".
+        // FIXME: iv. Let result be ? Call(exoticToPrim, input, « hint »).
+        // FIXME: v. If result is not an Object, return result.
+        // FIXME:  vi. Throw a TypeError exception.
+        // FIXME: c. If preferredType is not present, let preferredType be NUMBER.
+        // FIXME: d. Return? OrdinaryToPrimitive(input, preferredType).
+
+        // 2. Return input.
+        return Completion.NormalCompletion(this);
+    }
 }
