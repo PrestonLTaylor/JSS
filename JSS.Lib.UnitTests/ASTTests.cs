2448097033c7874f4a4a16a6833ab24377631542
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-11-24 22:44:23 +0000

    JSS.Lib: Implement execution for MultiplicationExpressions

--- JSS.Lib.UnitTests/ASTTests.cs
+++ JSS.Lib.UnitTests/ASTTests.cs
@@ -292,6 +292,27 @@ internal sealed class ASTTests
         Assert.That(completion.Value, Is.EqualTo(expectedValue));
     }
 
+    static private readonly object[] normalCompletionMultiplyTestCases =
+    {
+        new object[] { new NumericLiteral(1.0), new NumericLiteral(1.0), new Number(1.0) },
+        new object[] { new NumericLiteral(1.0), new NumericLiteral(-1.0), new Number(-1.0) },
+    };
+
+    [TestCaseSource(nameof(normalCompletionMultiplyTestCases))]
+    public void MultiplicationExpression_Evaluate_ReturnsNormalCompletion_WithExpectedResult(IExpression lhs, IExpression rhs, Value expectedValue)
+    {
+        // Arrange
+        var vm = new VM();
+        var multiplicationExpression = new MultiplicationExpression(lhs, rhs);
+
+        // Act
+        var completion = multiplicationExpression.Evaluate(vm);
+
+        // Assert
+        Assert.That(completion.IsNormalCompletion(), Is.True);
+        Assert.That(completion.Value, Is.EqualTo(expectedValue));
+    }
+
     static private readonly object[] normalCompletionSubtractionTestCases =
     {
         new object[] { new NumericLiteral(1.0), new NumericLiteral(1.0), new Number(0.0) },
