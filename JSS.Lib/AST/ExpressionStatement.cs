a99ae9d0a811682ab20f2f0811046d625b1fbd8b
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-11-26 14:53:11 +0000

    JSS.Lib: Implement execution of ExpressionStatements
    There is no unit tests for this commit as the next commit will change
    ASTTests to use the parser. So for testing literals we need execution of
    expression statements.

--- JSS.Lib/AST/ExpressionStatement.cs
+++ JSS.Lib/AST/ExpressionStatement.cs
@@ -1,4 +1,6 @@
-﻿namespace JSS.Lib.AST;
+﻿using JSS.Lib.Execution;
+
+namespace JSS.Lib.AST;
 
 // 14.5 Expression Statement, https://tc39.es/ecma262/#sec-expression-statement
 internal sealed class ExpressionStatement : INode
@@ -8,7 +10,15 @@ internal sealed class ExpressionStatement : INode
         Expression = expression;
     }
 
-    // FIXME: 14.5.1 Runtime Semantics: Evaluation, https://tc39.es/ecma262/#sec-expression-statement-runtime-semantics-evaluation
+    // 14.5.1 Runtime Semantics: Evaluation, https://tc39.es/ecma262/#sec-expression-statement-runtime-semantics-evaluation
+    override public Completion Evaluate(VM vm)
+    {
+        // 1. Let exprRef be ? Evaluation of Expression.
+        var exprRef = Expression.Evaluate(vm);
+
+        // FIXME: 2. Return ? GetValue(exprRef).
+        return exprRef;
+    }
 
     public IExpression Expression { get; }
 }
