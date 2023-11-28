2590544be7b181036beb3b710af67f812444aa7e
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-11-28 01:09:13 +0000

    JSS.Lib: Implement execution of DebuggerStatements

--- JSS.Lib/AST/DebuggerStatement.cs
+++ JSS.Lib/AST/DebuggerStatement.cs
@@ -1,7 +1,26 @@
-﻿namespace JSS.Lib.AST;
+﻿using JSS.Lib.Execution;
+using System.Diagnostics;
+
+namespace JSS.Lib.AST;
 
 // 14.16 The debugger Statement, https://tc39.es/ecma262/#sec-debugger-statement
 internal sealed class DebuggerStatement : INode
 {
-    // FIXME: 14.16.1 Runtime Semantics: Evaluation, https://tc39.es/ecma262/#sec-debugger-statement-runtime-semantics-evaluation
+    // 14.16.1 Runtime Semantics: Evaluation, https://tc39.es/ecma262/#sec-debugger-statement-runtime-semantics-evaluation
+    public override Completion Evaluate(VM vm)
+    {
+        // 1. If an implementation-defined debugging facility is available and enabled, then
+        if (Debugger.IsAttached)
+        {
+            // a. Perform an implementation-defined debugging action.
+            Debugger.Break();
+
+            // b. Return a new implementation-defined Completion Record.
+            return Completion.NormalCompletion(vm.Empty);
+        }
+
+        // 2. Else,
+        // a. Return EMPTY.
+        return Completion.NormalCompletion(vm.Empty);
+    }
 }
