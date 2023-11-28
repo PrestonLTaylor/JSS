9fbd9ef664d28a8eda2c565f724b309370067e72
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-11-28 16:14:53 +0000

    JSS.CLI: Implement a CLI for parsing and evaluating input

---
+++ JSS/Print.cs
@@ -0,0 +1,68 @@
+﻿using JSS.Lib;
+using JSS.Lib.AST.Values;
+using JSS.Lib.Execution;
+using Boolean = JSS.Lib.AST.Values.Boolean;
+using String = JSS.Lib.AST.Values.String;
+
+namespace JSS.CLI;
+
+internal sealed class Print
+{
+    static public string PrintCompletion(Completion completion)
+    {
+        if (completion.IsThrowCompletion())
+        {
+            // FIXME: Print the exception type
+            return $"Uncaught {PrintValue(completion.Value)}";
+        }
+        else
+        {
+            return PrintValue(completion.Value);
+        }
+    }
+
+    static public string PrintValue(Value value)
+    {
+        if (value.IsEmpty())
+        {
+            return "EMPTY";
+        }
+        else if (value.IsUndefined())
+        {
+            return "undefined";
+        }
+        else if (value.IsNull())
+        {
+            return "null";
+        }
+        else if (value.IsBoolean())
+        {
+            var asBoolean = (value as Boolean)!;
+            return asBoolean.Value ? "true" : "false";
+        }
+        else if (value.IsString())
+        {
+            var asString = (value as String)!;
+            return $"\"{asString.Value}\"";
+        }
+        else if (value.IsNumber())
+        {
+            var asNumber = (value as Number)!;
+            return asNumber.Value.ToString();
+        }
+
+        return value.Type().ToString();
+    }
+
+    static public string PrintException(Exception e)
+    {
+        if (e is SyntaxErrorException)
+        {
+            return $"Uncaught {e.Message}";
+        }
+        else
+        {
+            return $"Internal Error: {e.Message}";
+        }
+    }
+}
--- JSS/Program.cs
+++ JSS/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, JSS!");
+﻿using JSS.Lib;
+using JSS.CLI;
+
+// FIXME: We currently use "InternalsVisibleTo" for JSS.Lib, we should fix that
+while (true)
+{
+    Console.Write("> ");
+
+    try
+    {
+        var input = Console.ReadLine() ?? "";
+        var parser = new Parser(input);
+        var script = parser.Parse();
+        var result = script.ScriptEvaluation();
+
+        if (result.IsThrowCompletion())
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+        }
+        Console.ForegroundColor = ConsoleColor.White;
+
+        Console.Write(Print.PrintCompletion(result));
+    }
+    catch (Exception e)
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.White;
+
+        Console.Write(Print.PrintException(e));
+    }
+
+    Console.ResetColor();
+
+    // NOTE: We need this empty write line to prevent the background color from spilling over
+    Console.WriteLine();
+}
