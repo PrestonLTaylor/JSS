eb03657d34898cb37c51715e49db56c73b315558
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-12-04 12:54:46 +0000

    JSS.Lib: Implement the NewDeclarativeEnvironment Abstract Operation

--- JSS.Lib/Execution/DeclarativeEnvironment.cs
+++ JSS.Lib/Execution/DeclarativeEnvironment.cs
@@ -3,9 +3,15 @@
 // 9.1.1.1 Declarative Environment Records, https://tc39.es/ecma262/#sec-declarative-environment-records
 internal sealed class DeclarativeEnvironment : Environment
 {
+    // 9.1.2.2 NewDeclarativeEnvironment ( E ), https://tc39.es/ecma262/#sec-newdeclarativeenvironment
     public DeclarativeEnvironment(Environment? outerEnv)
     {
+        // 1. Let env be a new Declarative Environment Record containing no bindings.
+
+        // 2. Set env.[[OuterEnv]] to E.
         OuterEnv = outerEnv;
+
+        // 3. Return env.
     }
 
     // 9.1.1.1.1 HasBinding ( N ), https://tc39.es/ecma262/#sec-declarative-environment-records
