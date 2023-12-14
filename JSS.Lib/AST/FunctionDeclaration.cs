427ce58b08366327188e99a4c807bc6f2c031947
PrestonLTaylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-12-13 23:51:22 -0500

    JSS.Lib: Implement the InstantiateFunctionObject abstract operation

--- JSS.Lib/AST/FunctionDeclaration.cs
+++ JSS.Lib/AST/FunctionDeclaration.cs
@@ -1,4 +1,7 @@
-﻿namespace JSS.Lib.AST;
+﻿using JSS.Lib.AST.Values;
+using Environment = JSS.Lib.Execution.Environment;
+
+namespace JSS.Lib.AST;
 
 // 15.2 Function Definitions, https://tc39.es/ecma262/#prod-FunctionDeclaration
 internal sealed class FunctionDeclaration : Declaration
@@ -16,6 +19,27 @@ internal sealed class FunctionDeclaration : Declaration
     {
         // 1. Return the BoundNames of BindingIdentifier.
         return new List<string> { Identifier };
+    }
+
+    // 8.6.1 Runtime Semantics: InstantiateFunctionObject, https://tc39.es/ecma262/#sec-runtime-semantics-instantiatefunctionobject
+    public FunctionObject InstantiateFunctionObject(Environment env)
+    {
+        // NOTE/FIXME: This is the steps for InstantiateOrdinaryFunctionObject
+
+        // 1. Let name be StringValue of BindingIdentifier.
+
+        // FIXME: 2. Let sourceText be the source text matched by FunctionDeclaration.
+
+        // FIXME: 3. Let F be OrdinaryFunctionCreate(%Function.prototype%, sourceText, FormalParameters, FunctionBody, non-lexical-this, env, privateEnv).
+        var F = new FunctionObject(Parameters, Body);
+
+        // 4. Perform SetFunctionName(F, name).
+        F.SetFunctionName(Identifier);
+
+        // FIXME: 5. Perform MakeConstructor(F).
+
+        // 6. Return F.
+        return F;
     }
 
     // FIXME: 15.2.6 Runtime Semantics: Evaluation, https://tc39.es/ecma262/#sec-function-definitions-runtime-semantics-evaluation
