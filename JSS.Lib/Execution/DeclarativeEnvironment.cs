3cee8fb8b74149dc540d6d9dbe2d312137bb4b76
PrestonLTaylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-12-30 23:18:14 -0500

    JSS.Lib: Implement the FunctionEnvironment

--- JSS.Lib/Execution/DeclarativeEnvironment.cs
+++ JSS.Lib/Execution/DeclarativeEnvironment.cs
@@ -5,7 +5,7 @@ using String = JSS.Lib.AST.Values.String;
 namespace JSS.Lib.Execution;
 
 // 9.1.1.1 Declarative Environment Records, https://tc39.es/ecma262/#sec-declarative-environment-records
-internal sealed class DeclarativeEnvironment : Environment
+internal class DeclarativeEnvironment : Environment
 {
     // 9.1.2.2 NewDeclarativeEnvironment ( E ), https://tc39.es/ecma262/#sec-newdeclarativeenvironment
     public DeclarativeEnvironment(Environment? outerEnv)
---
+++ JSS.Lib/Execution/FunctionEnvironment.cs
@@ -0,0 +1,30 @@
+﻿using JSS.Lib.AST.Values;
+using Object = JSS.Lib.AST.Values.Object;
+
+namespace JSS.Lib.Execution;
+
+// 9.1.1.3 Function Environment Records, https://tc39.es/ecma262/#sec-function-environment-records
+internal class FunctionEnvironment : DeclarativeEnvironment
+{
+    // 9.1.2.4 NewFunctionEnvironment ( F, newTarget ), https://tc39.es/ecma262/#sec-newfunctionenvironment
+    public FunctionEnvironment(FunctionObject F, Object newTarget) : base(F.Environment)
+    {
+        // 1. Let env be a new Function Environment Record containing no bindings.
+        // 2. Set env.[[FunctionObject]] to F.
+        FunctionObject = F;
+
+        // FIXME: 3. If F.[[ThisMode]] is LEXICAL, set env.[[ThisBindingStatus]] to LEXICAL.
+        // FIXME: 4. Else, set env.[[ThisBindingStatus]] to UNINITIALIZED.
+
+        // 5. Set env.[[NewTarget]] to newTarget.
+        NewTarget = newTarget;
+
+        // NOTE: Performed in the base constructor call
+        // 6. Set env.[[OuterEnv]] to F.[[Environment]].
+
+        // 7. Return env.
+    }
+
+    public FunctionObject FunctionObject { get; }
+    public Object NewTarget { get; }
+}
