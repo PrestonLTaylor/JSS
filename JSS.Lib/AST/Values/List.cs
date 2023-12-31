b7f271d6f69193208b77ff01981f39afe966a783
PrestonLTaylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-12-30 23:25:29 -0500

    JSS.Lib: Implement a List of values that inherits from JS value

---
+++ JSS.Lib/AST/Values/List.cs
@@ -0,0 +1,11 @@
+﻿namespace JSS.Lib.AST.Values;
+
+internal sealed class List : Value
+{
+    override public ValueType Type() { throw new InvalidOperationException("Tried to get the Type of List"); }
+
+    public void Add(Value value) => Values.Add(value);
+    public void ListConcatenation(List list) => Values.AddRange(list.Values);
+
+    public List<Value> Values { get; } = new();
+}
