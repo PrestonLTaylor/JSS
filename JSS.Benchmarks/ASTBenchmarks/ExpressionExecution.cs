8a4ad9b89954606caa0bb9ed673e8f61cabb9b79
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-04-05 16:03:42 +0100

    feat: Add a basic benchmark suite for expression parsing and execution
    We add a basic benchmark suite that parses and executes some basic
    expressions.
    
    This is currently just a toy to see how fast JSS.Lib is and can be
    expanded further when we are able to do more.

---
+++ JSS.Benchmarks/ASTBenchmarks/ExpressionExecution.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using JSS.Lib;
+using JSS.Lib.Execution;
+
+namespace JSS.Benchmarks.ParserBenchmarks;
+
+public class ExpressionExecution
+{
+    [GlobalSetup]
+    public void SetupVM()
+    {
+        var completion = Realm.InitializeHostDefinedRealm(out VM vm);
+        if (completion.IsAbruptCompletion())
+        {
+            throw new InvalidOperationException("Unable to initialize a host defined realm");
+        }
+
+        var parser = new Parser(Expression);
+        _script = parser.Parse(vm);
+    }
+
+    [Benchmark]
+    public Completion ParseExpression()
+    {
+        return _script.ScriptEvaluation();
+    }
+
+    [ParamsSource(nameof(Expressions))]
+    public string Expression;
+    public string[] Expressions { get; } =
+    [
+        "1 || 2",
+        "1 || 2 && 3",
+        "1 || 2 && 3 | 4 & 5 ** 6",
+        "((((1 + 2) * 3) / 4) !== 5)",
+        "((((1 + 2) * 3) / 4) !== 5) + ((((1 + 2) * 3) / 4) !== 5) + ((((1 + 2) * 3) / 4) !== 5) + ((((1 + 2) * 3) / 4) !== 5) + ((((1 + 2) * 3) / 4) !== 5)",
+    ];
+
+    private Script _script;
+}
---
+++ JSS.Benchmarks/ParserBenchmarks/ExpressionParsing.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using JSS.Lib;
+using JSS.Lib.Execution;
+
+namespace JSS.Benchmarks.ParserBenchmarks;
+
+public class ExpressionParsing
+{
+    [GlobalSetup]
+    public void SetupVM()
+    {
+        var completion = Realm.InitializeHostDefinedRealm(out _vm);
+        if (completion.IsAbruptCompletion())
+        {
+            throw new InvalidOperationException("Unable to initialize a host defined realm");
+        }
+
+        _parser = new Parser(Expression);
+    }
+
+    [Benchmark]
+    public Script ParseExpression()
+    {
+        return _parser.Parse(_vm);
+    }
+
+    [ParamsSource(nameof(Expressions))]
+    public string Expression;
+    public string[] Expressions { get; } =
+    [
+        "1 || 2",
+        "1 || 2 && 3",
+        "1 || 2 && 3 | 4 & 5 ** 6",
+        "((((1 + 2) * 3) / 4) !== 5)",
+        "((((1 + 2) * 3) / 4) !== 5) + ((((1 + 2) * 3) / 4) !== 5) + ((((1 + 2) * 3) / 4) !== 5) + ((((1 + 2) * 3) / 4) !== 5) + ((((1 + 2) * 3) / 4) !== 5)",
+    ];
+
+    private VM _vm;
+    private Parser _parser;
+}
