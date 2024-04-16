edb0d0860f3bbcd59150bce674150eecf9e4c32a
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-04-16 20:36:50 +0100

    feat: Add a class for executing a single javascript file

---
+++ JSS/FileExecutor.cs
@@ -0,0 +1,35 @@
+﻿using JSS.Lib;
+using JSS.Lib.Execution;
+
+namespace JSS.CLI;
+
+internal sealed class FileExecutor
+{
+    public FileExecutor(VM globalVm, string filePath)
+    {
+        _vm = globalVm;
+        _filePath = filePath;
+    }
+
+    public void ExecuteFile()
+    {
+        try
+        {
+            var fileContent = File.ReadAllText(_filePath);
+            var parser = new Parser(fileContent);
+            var script = parser.Parse(_vm);
+            var result = script.ScriptEvaluation();
+            Print.PrintCompletion(result);
+        }
+        catch (Exception e)
+        {
+            Print.PrintException(e);
+        }
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+
+    private readonly VM _vm;
+    private readonly string _filePath;
+}
