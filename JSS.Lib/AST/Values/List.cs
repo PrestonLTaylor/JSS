2911c45a81d0f75e119cd05d898724e968275d35
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-04-20 18:14:25 +0100

    JSS-6 Implement constructing a List from an IEnumerable<Value>
    This is needed in call as we want to pass all but the first argument to
    the function being called.

--- JSS.Lib/AST/Values/List.cs
+++ JSS.Lib/AST/Values/List.cs
@@ -2,6 +2,12 @@
 
 internal sealed class List : Value
 {
+    public List() { }
+    public List(IEnumerable<Value> values)
+    {
+        Values = values.ToList();
+    }
+
     override public ValueType Type() { throw new InvalidOperationException("Tried to get the Type of List"); }
 
     public void Add(Value value) => Values.Add(value);
