94fee97e99d3e7a4889b36a1ac3708df62d8d458
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-04-20 18:24:54 +0100

    JSS-6 Implement the call function on the Function prototype
    This allows for functions to be called using the call function. A new
    thisArgument and args can be provided to the call.

--- JSS.Lib/Runtime/Function.prototype.cs
+++ JSS.Lib/Runtime/Function.prototype.cs
@@ -1,4 +1,7 @@
-﻿namespace JSS.Lib.Runtime;
+﻿using JSS.Lib.AST.Values;
+using JSS.Lib.Execution;
+
+namespace JSS.Lib.Runtime;
 
 // 20.2.3 Properties of the Function Prototype Object, https://tc39.es/ecma262/#sec-properties-of-the-function-prototype-object
 internal sealed class FunctionPrototype : Object
@@ -6,5 +9,29 @@ internal sealed class FunctionPrototype : Object
     // The Function prototype has a [[Prototype]] internal slot whose value is %Object.prototype%.
     public FunctionPrototype(ObjectPrototype objectPrototype) : base(objectPrototype)
     {
+    }
+
+    public void Initialize(VM vm)
+    {
+        // 20.2.3.3 Function.prototype.call ( thisArg, ...args ), https://tc39.es/ecma262/#sec-function.prototype.call
+        var callBuiltin = BuiltinFunction.CreateBuiltinFunction(vm, call);
+        DataProperties.Add("call", new Property(callBuiltin, new(true, false, true)));
+    }
+
+    // 20.2.3.3 Function.prototype.call ( thisArg, ...args ), https://tc39.es/ecma262/#sec-function.prototype.call
+    private Completion call(VM vm, Value? thisArg, List argumentList)
+    {
+        // 1. Let func be the this value.
+        var func = thisArg;
+
+        // 2. If IsCallable(func) is false, throw a TypeError exception.
+        if (func is null || !func.IsCallable()) return ThrowTypeError(vm, RuntimeErrorType.CallingANonFunction, func?.Type() ?? ValueType.Undefined);
+
+        // FIXME: 3. Perform PrepareForTailCall().
+
+        // 4. Return ? Call(func, thisArg, args).
+        var newThisArg = argumentList[0];
+        var args = new List(argumentList.Values.Skip(1));
+        return Call(vm, func, newThisArg, args);
     }
 }
