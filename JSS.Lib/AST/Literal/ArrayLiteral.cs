bf4f1af3d4ce9882eb2fbebc58718078b8c796e6
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-04-23 00:42:51 +0100

    JSS-16 Implement execution of the ArrayLiteral AST Node
    Currently, we only have empty array literal ast nodes. So we always
    create an array with length 0 and no values.

--- JSS.Lib/AST/Literal/ArrayLiteral.cs
+++ JSS.Lib/AST/Literal/ArrayLiteral.cs
@@ -1,5 +1,17 @@
-﻿namespace JSS.Lib.AST.Literal;
+﻿using JSS.Lib.Execution;
+
+namespace JSS.Lib.AST.Literal;
 
 internal sealed class ArrayLiteral : IExpression
 {
+    // 13.2.4.2 Runtime Semantics: Evaluation, https://tc39.es/ecma262/#sec-array-initializer-runtime-semantics-evaluation
+    override public Completion Evaluate(VM vm)
+    {
+        // FIXME: Implement the rest of the evaluation when we parse element lists
+        // 1. Let array be ! ArrayCreate(0).
+        var array = MUST(Array.ArrayCreate(0));
+
+        // 3. Return array.
+        return array;
+    }
 }
