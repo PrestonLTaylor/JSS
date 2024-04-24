2836599e32cd500fd684c77b18ef6f6639a42ec4
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-04-24 02:05:05 +0100

    JSS-24 Create an empty ArrayPrototype class

---
+++ JSS.Lib/Runtime/Array.prototype.cs
@@ -0,0 +1,9 @@
+﻿namespace JSS.Lib.Runtime;
+
+internal sealed class ArrayPrototype : Object
+{
+    // The Array prototype object has a [[Prototype]] internal slot whose value is %Object.prototype%.
+    public ArrayPrototype(ObjectPrototype prototype) : base(prototype)
+    {
+    }
+}
