84fab050de2831c4d93001f416bf197c3feb3016
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-04-25 22:49:14 +0100

    JSS-26 Implement execution of ternary expressions
    We now can parse and execute ternary expressions.
    
    Example Code:
    ```js
    var a = true ? 1 : 2; // a holds 1
    var b = false ? 1 : 2; // a holds 2
    ```

--- JSS.Lib/AST/TernaryExpression.cs
+++ JSS.Lib/AST/TernaryExpression.cs
@@ -1,4 +1,6 @@
-﻿namespace JSS.Lib.AST;
+﻿using JSS.Lib.Execution;
+
+namespace JSS.Lib.AST;
 
 // 13.14 Conditional Operator ( ? : ), https://tc39.es/ecma262/#prod-ConditionalExpression
 internal sealed class TernaryExpression : IExpression
@@ -8,6 +10,40 @@ internal sealed class TernaryExpression : IExpression
         TestExpression = testExpression;
         TrueCaseExpression = trueCaseExpression;
         FalseCaseExpression = falseCaseExpression;
+    }
+
+    // 13.14.1 Runtime Semantics: Evaluation, https://tc39.es/ecma262/#prod-ConditionalExpression
+    public override Completion Evaluate(VM vm)
+    {
+        // 1. Let lref be ? Evaluation of ShortCircuitExpression.
+        var lref = TestExpression.Evaluate(vm);
+        if (lref.IsAbruptCompletion()) return lref;
+
+        // 2. Let lval be ToBoolean(? GetValue(lref)).
+        var getResult = lref.Value.GetValue(vm);
+        if (getResult.IsAbruptCompletion()) return getResult;
+        var lval = getResult.Value.ToBoolean();
+
+        // 3. If lval is true, then
+        if (lval)
+        {
+            // a. Let trueRef be ? Evaluation of the first AssignmentExpression.
+            var trueRef = TrueCaseExpression.Evaluate(vm);
+            if (trueRef.IsAbruptCompletion()) return trueRef;
+
+            // b. Return ? GetValue(trueRef).
+            return trueRef.Value.GetValue(vm);
+        }
+        // 4. Else,
+        else
+        {
+            // a. Let falseRef be ? Evaluation of the second AssignmentExpression.
+            var falseRef = FalseCaseExpression.Evaluate(vm);
+            if (falseRef.IsAbruptCompletion()) return falseRef;
+
+            // b. Return ? GetValue(falseRef).
+            return falseRef.Value.GetValue(vm);
+        }
     }
 
     public IExpression TestExpression { get; }
