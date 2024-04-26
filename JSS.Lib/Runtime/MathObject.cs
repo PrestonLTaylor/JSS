2e0b8de63793d3ad2c3d1d86d5949f2b8a0de506
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-04-26 15:35:16 +0100

    JSS-30 Implement the pow function on the MathObject
    This allows for the pow function to be called from the math object. This
    function calculate the power of two numbers.
    
    Example Code:
    ```js
    Math.pow(2, 2); // Returns 4
    Math.pow(2, 3); // Returns 8
    ```

--- JSS.Lib/Runtime/MathObject.cs
+++ JSS.Lib/Runtime/MathObject.cs
@@ -1,4 +1,7 @@
-﻿namespace JSS.Lib.Runtime;
+﻿using JSS.Lib.AST.Values;
+using JSS.Lib.Execution;
+
+namespace JSS.Lib.Runtime;
 
 // 21.3 The Math Object, https://tc39.es/ecma262/#sec-math-object
 internal sealed class MathObject : Object
@@ -6,5 +9,27 @@ internal sealed class MathObject : Object
     // The Math object has a [[Prototype]] internal slot whose value is %Object.prototype%.
     public MathObject(ObjectPrototype prototype) : base(prototype)
     {
+    }
+
+    public void Initialize(VM vm)
+    {
+        // 21.3.2.26 Math.pow ( base, exponent ), https://tc39.es/ecma262/#sec-math.pow
+        var powBuiltin = BuiltinFunction.CreateBuiltinFunction(vm, pow);
+        DataProperties.Add("pow", new(powBuiltin, new(true, false, true)));
+    }
+
+    // 21.3.2.26 Math.pow ( base, exponent ), https://tc39.es/ecma262/#sec-math.pow
+    private Completion pow(VM vm, Value? thisArgument, List argumentList)
+    {
+        // 1. Set base to ? ToNumber(base).
+        var powBase = argumentList[0].ToNumber();
+        if (powBase.IsAbruptCompletion()) return powBase.Completion;
+
+        // 2. Set exponent to ? ToNumber(exponent).
+        var exponent = argumentList[1].ToNumber();
+        if (exponent.IsAbruptCompletion()) return exponent.Completion;
+
+        // 3. Return Number::exponentiate(base, exponent).
+        return Number.Exponentiate(powBase.Value, exponent.Value);
     }
 }
