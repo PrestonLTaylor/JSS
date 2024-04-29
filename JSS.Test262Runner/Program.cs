a75ae9cf1c755e6f0219fe45d28f209988518a23
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-04-29 21:02:13 +0100

    JSS-39 Report harness execution failures separetely
    We now report if we cannot successfully create a VM for a test case as
    harness execution failures, if we could not parse or successfully
    execute a harness file.

--- JSS.Test262Runner/Program.cs
+++ JSS.Test262Runner/Program.cs
@@ -1,12 +1,12 @@
 ﻿// TODO: Setup ILogger (or something similar) that we can use for this project
 using JSS.Test262Runner;
+using System.Text;
 
 var test262RepositoryCloner = new GitHubCloner("tc39/test262");
 test262RepositoryCloner.CloneRepositoryIfNotAlreadyPresent();
 
+Console.OutputEncoding = Encoding.UTF8;
+
 Console.WriteLine("\nStarting the test-262 runner...");
 var test262Runner = new Test262Runner();
 test262Runner.StartRunner();
-
-Console.WriteLine("\nPress ENTER to exit.");
-Console.ReadLine();
