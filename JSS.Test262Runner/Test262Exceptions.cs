92985a2d1912a82b198219b560c8756bd09ec09d
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-04-29 22:09:35 +0100

    JSS-39 Print the reason why a test failed for every test case

--- JSS.Test262Runner/Test262Exceptions.cs
+++ JSS.Test262Runner/Test262Exceptions.cs
@@ -12,26 +12,9 @@ internal class Test262Exception : Exception
 {
     protected Test262Exception(string message) : base(message) { }
 
-    protected Test262Exception(VM vm, Completion abruptCompletion) : base(CreateTest262ExceptionMessage(vm, abruptCompletion))
+    protected Test262Exception(VM vm, Completion abruptCompletion) : base(Print.CompletionToString(vm, abruptCompletion))
     {
         Debug.Assert(abruptCompletion.IsAbruptCompletion());
-    }
-
-    static private string CreateTest262ExceptionMessage(VM vm, Completion abruptCompletion)
-    {
-        var completionType = CompletionTypeToString(abruptCompletion);
-        var completionValue = Print.CompletionToString(vm, abruptCompletion);
-
-        return $"Completion Type: {completionType} Value: {completionValue}";
-    }
-
-    static private string CompletionTypeToString(Completion completion)
-    {
-        if (completion.IsThrowCompletion()) return "Throw Completion";
-        if (completion.IsReturnCompletion()) return "Return Completion";
-        if (completion.IsBreakCompletion()) return "Break Completion";
-        if (completion.IsContinueCompletion()) return "Continue Completion";
-        return "Normal Completion";
     }
 }
 
