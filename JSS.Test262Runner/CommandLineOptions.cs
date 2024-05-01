1dc9180e85625ec2f5becb5f712d0277764f2b5c
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-05-01 01:16:35 +0100

    JSS-44 Implement a command line argument for filtering tests
    We now have a command line argument for filtering tests "-f" or
    "--filter". This is useful for only running test-262 on a specific set
    of tests that are being actively worked on.

--- JSS.Test262Runner/CommandLineOptions.cs
+++ JSS.Test262Runner/CommandLineOptions.cs
@@ -7,4 +7,8 @@ internal sealed class CommandLineOptions
     [Option('q', "quiet", FlagCounter = true, HelpText = "Enables quiet mode. Won't output results for every test executed.")]
     public int QuietCount { get; set; }
     public bool Quiet => QuietCount > 0;
+
+    [Option('f', "filter", Default = "*.js",
+        HelpText = "Filters tests to execute based on their path. * is a wildcard for zero or more characters and ? is a wildcard for zero or one characters.")]
+    public required string Filter { get; set; }
 }
