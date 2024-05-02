b0860e04c18d264f7a9cfe57fe7105a8ec44f7d7
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-05-02 19:10:40 +0100

    JSS-47 Implement a trie that stores test results by sub-directories
    This implements a trie that stores test results by their path from each
    sub-directory.
    
    For example the result for "/a/b/c/test.js" would be stored in "/a/",
    "/a/b/" and "/a/b/c/".

---
+++ JSS.Test262Runner/TestResultPathTrie.cs
@@ -0,0 +1,60 @@
+﻿namespace JSS.Test262Runner;
+
+// FIXME: Change strings through-out code base to spans where applicable.
+/// <summary>
+/// Implements a trie based on splitting a path by its directories for storing test results.
+/// </summary>
+internal sealed class TestResultPathTrie
+{
+    /// <summary>
+    /// Adds a test result to all the nodes that the path visits.
+    /// </summary>
+    /// <param name="path">The path to the test case file.</param>
+    /// <param name="result">The result of the test.</param>
+    public void Add(string path, TestResultType result)
+    {
+        _testResults.TryAdd(result, 0);
+        _testResults[result]++;
+
+        var nextPathSeperator = path.IndexOf(Path.DirectorySeparatorChar);
+        if (nextPathSeperator == -1) return;
+
+        var currentDirectory = path[..nextPathSeperator];
+        if (!_leaves.ContainsKey(currentDirectory)) _leaves.Add(currentDirectory, new());
+
+        var leaf = _leaves[currentDirectory];
+        var nextPath = path[(nextPathSeperator + 1)..];
+        leaf.Add(nextPath, result);
+    }
+
+    /// <summary>
+    /// Visits every node in the trie using DFS and calls the <paramref name="visitor"/> callabck.
+    /// </summary>
+    /// <param name="visitor">A visitor that is called on each node of the trie.</param>
+    /// <param name="path">The current path for the current node.</param>
+    public void Visit(Action<string, Dictionary<TestResultType, int>> visitor, string path = "/")
+    {
+        if (path != "/") visitor(path, _testResults);
+
+        foreach (var (nextDirectory, trieNode) in _leaves)
+        {
+            trieNode.Visit(visitor, path + $"/{nextDirectory}");
+        }
+    }
+
+    static private Dictionary<TestResultType, int> CreateZeroedResultsDictionary()
+    {
+        return new()
+        {
+            { TestResultType.SUCCESS, 0 },
+            { TestResultType.METADATA_PARSING_FAILURE, 0 },
+            { TestResultType.HARNESS_EXECUTION_FAILURE, 0 },
+            { TestResultType.PARSING_FAILURE, 0 },
+            { TestResultType.CRASH_FAILURE, 0 },
+            { TestResultType.FAILURE, 0 },
+        };
+    }
+
+    private readonly Dictionary<TestResultType, int> _testResults = CreateZeroedResultsDictionary(); 
+    private readonly Dictionary<string, TestResultPathTrie> _leaves = [];
+}
