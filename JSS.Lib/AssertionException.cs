850fa0e6a06cd7667e1c8a89266640ec5c7deee6
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-05-02 21:34:50 +0100

    JSS-48 Implement an "AssertionException" class with an Assert method
    We now have an AssertionException exception class that we can throw in
    place of Debug.Asserts.
    
    We also have the static Assert method that takes a boolean and a message
    and imitates an assertion, throwing if the boolean is false and contains
    the message.

---
+++ JSS.Lib/AssertionException.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace JSS.Lib;
+
+internal sealed class AssertionException : Exception
+{
+    public AssertionException() : base() { }
+    public AssertionException(string? message) : base(message) { }
+    public AssertionException(string? message, Exception? innerException) : base(message, innerException) { }
+
+    static public void Assert([DoesNotReturnIf(false)] bool shouldBeTrue, string message)
+    {
+        if (shouldBeTrue) return;
+
+        throw new AssertionException($"Assertion Failed! {message}");
+    }
+}
