b1eca9e8d5228830ef81b8cc3fa8c1854331f11b
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-05-03 16:22:52 +0100

    chore: Only try to clone test-262 repository when running test runner
    We would always try to clone the test262 git repository even if we were
    running our test differ.

--- JSS.Test262Runner/Program.cs
+++ JSS.Test262Runner/Program.cs
@@ -3,19 +3,21 @@ using CommandLine;
 using JSS.Test262Runner;
 using System.Text;
 
-var test262RepositoryCloner = new GitHubCloner("tc39/test262");
-test262RepositoryCloner.CloneRepositoryIfNotAlreadyPresent();
 
 Console.OutputEncoding = Encoding.UTF8;
 
 Parser.Default.ParseArguments<DiffOptions, RunnerOptions>(args)
     .WithParsed<DiffOptions>(options =>
     {
+        Console.WriteLine("\nStarting the test-262 differ...");
         var differ = new TestRunDiffer(options);
         differ.LogTestsDifferences();
     })
     .WithParsed<RunnerOptions>(options =>
     {
+        var test262RepositoryCloner = new GitHubCloner("tc39/test262");
+        test262RepositoryCloner.CloneRepositoryIfNotAlreadyPresent();
+
         Console.WriteLine("\nStarting the test-262 runner...");
         var test262Runner = new Test262Runner(options);
         test262Runner.StartRunner();
