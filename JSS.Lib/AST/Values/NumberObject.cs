92180768286e567a57d7f395f2ecced4c4b53f08
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-05-03 14:09:56 +0100

    JSS-49 Implement the NumberObject object

---
+++ JSS.Lib/AST/Values/NumberObject.cs
@@ -0,0 +1,16 @@
+﻿using JSS.Lib.Runtime;
+
+namespace JSS.Lib.AST.Values;
+
+// 21.1.4 Properties of Number Instances, https://tc39.es/ecma262/#sec-properties-of-number-instances
+internal sealed class NumberObject : Object
+{
+    // FIXME: Number instances are ordinary objects that inherit properties from the Number prototype object.
+    public NumberObject(ObjectPrototype prototype, Number value) : base(prototype)
+    {
+        NumberData = value;
+    }
+
+    // [[NumberData]]
+    public Number NumberData { get; }
+}
