eb1a7bbf15fb2a141eba0bba2ab3430acf043351
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-05-03 14:04:40 +0100

    JSS-49 Implement the BooleanObject object

---
+++ JSS.Lib/AST/Values/BooleanObject.cs
@@ -0,0 +1,16 @@
+﻿using JSS.Lib.Runtime;
+
+namespace JSS.Lib.AST.Values;
+
+// 20.3.4 Properties of Boolean Instances, https://tc39.es/ecma262/#sec-properties-of-boolean-instances
+internal sealed class BooleanObject : Object
+{
+    // FIXME: Boolean instances are ordinary objects that inherit properties from the Boolean prototype object.
+    public BooleanObject(ObjectPrototype prototype, Boolean value) : base(prototype)
+    {
+        BooleanData = value;
+    }
+
+    // [[BooleanData]]
+    public Boolean BooleanData { get; }
+}
