05f8fa546e8f586f349e83711c9d786c2b1d4b7b
PrestonLTaylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-05-15 19:26:19 +0100

    JSS-74 Implement execution of arrow functions expressions
    We now support parsing and execution of arrow function expression.
    
    This allows for use of arrow functions inside of JSS.
    
    Example Code:
    ```js
    var c = a => b => a + b; // A complex arrow function closure
    c(1)(2); // Returns 3 as expected
    ```

--- JSS.Lib/AST/ArrowFunctionExpression.cs
+++ JSS.Lib/AST/ArrowFunctionExpression.cs
@@ -1,4 +1,7 @@
-﻿namespace JSS.Lib.AST;
+﻿using JSS.Lib.AST.Values;
+using JSS.Lib.Execution;
+
+namespace JSS.Lib.AST;
 
 // 15.3 Arrow Function Definitions, https://tc39.es/ecma262/#sec-arrow-function-definitions
 internal sealed class ArrowFunctionExpression : IExpression
@@ -8,6 +11,36 @@ internal sealed class ArrowFunctionExpression : IExpression
         Parameters = parameters;
         Body = body;
         IsStrict = isStrict;
+    }
+
+
+    // 15.3.4 Runtime Semantics: InstantiateArrowFunctionExpression, https://tc39.es/ecma262/#sec-runtime-semantics-instantiatearrowfunctionexpression
+    private Completion InstantiateArrowFunctionExpression(VM vm, string name = "")
+    {
+        // 1. If name is not present, set name to "".
+
+        // 2. Let env be the LexicalEnvironment of the running execution context.
+        var scriptExecutionContext = (vm.CurrentExecutionContext as ScriptExecutionContext)!;
+        var env = scriptExecutionContext.LexicalEnvironment;
+
+        // FIXME: 3. Let privateEnv be the running execution context's PrivateEnvironment.
+        // FIXME: 4. Let sourceText be the source text matched by ArrowFunction.
+
+        // 5. Let closure be OrdinaryFunctionCreate(%Function.prototype%, sourceText, ArrowParameters, ConciseBody, lexical-this, env, privateEnv).
+        var closure = FunctionObject.OrdinaryFunctionCreate(vm.FunctionPrototype, Parameters, Body, LexicalThisMode.LEXICAL_THIS, env!, IsStrict);
+
+        // 6. Perform SetFunctionName(closure, name).
+        FunctionObject.SetFunctionName(vm, closure, name);
+
+        // 7. Return closure.
+        return closure;
+    }
+
+    // 15.3.5 Runtime Semantics: Evaluation, https://tc39.es/ecma262/#sec-arrow-function-definitions-runtime-semantics-evaluation
+    public override Completion Evaluate(VM vm)
+    {
+        // 1. Return InstantiateArrowFunctionExpression of ArrowFunction.
+        return InstantiateArrowFunctionExpression(vm);
     }
 
     public List<Identifier> Parameters { get; }
--- JSS.Lib/AST/ExpressionBody.cs
+++ JSS.Lib/AST/ExpressionBody.cs
@@ -1,4 +1,6 @@
-﻿namespace JSS.Lib.AST;
+﻿using JSS.Lib.Execution;
+
+namespace JSS.Lib.AST;
 
 // 15.3 Arrow Function Definitions, https://tc39.es/ecma262/#sec-arrow-function-definitions
 internal sealed class ExpressionBody : INode
@@ -6,6 +8,21 @@ internal sealed class ExpressionBody : INode
     public ExpressionBody(IExpression expression)
     {
         Expression = expression;
+    }
+
+    // 15.3.5 Runtime Semantics: Evaluation, https://tc39.es/ecma262/#sec-arrow-function-definitions-runtime-semantics-evaluation
+    public override Completion Evaluate(VM vm)
+    {
+        // 1. Let exprRef be ? Evaluation of AssignmentExpression.
+        var exprRef = Expression.Evaluate(vm);
+        if (exprRef.IsAbruptCompletion()) return exprRef;
+
+        // 2. Let exprValue be ? GetValue(exprRef).
+        var exprValue = exprRef.Value.GetValue(vm);
+        if (exprValue.IsAbruptCompletion()) return exprValue;
+
+        // 3. Return Completion Record { [[Type]]: return, [[Value]]: exprValue, [[Target]]: empty }.
+        return Completion.ReturnCompletion(exprValue.Value);
     }
 
     public IExpression Expression { get; }
