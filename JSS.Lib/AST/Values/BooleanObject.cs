fc670240f5a03789ccb80a42698a9a2daf6868d2
PrestonLTaylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-05-17 14:43:34 +0100

    JSS-82 Implement an empty boolean prototype class

--- JSS.Lib/AST/Values/BooleanObject.cs
+++ JSS.Lib/AST/Values/BooleanObject.cs
@@ -3,7 +3,7 @@
 namespace JSS.Lib.AST.Values;
 
 // 20.3.4 Properties of Boolean Instances, https://tc39.es/ecma262/#sec-properties-of-boolean-instances
-internal sealed class BooleanObject : Object
+internal class BooleanObject : Object
 {
     // FIXME: Boolean instances are ordinary objects that inherit properties from the Boolean prototype object.
     public BooleanObject(ObjectPrototype prototype, Boolean value) : base(prototype)
---
+++ JSS.Lib/Runtime/Boolean.prototype.cs
@@ -0,0 +1,13 @@
+﻿using JSS.Lib.AST.Values;
+
+namespace JSS.Lib.Runtime;
+
+// 20.3.3 Properties of the Boolean Prototype Object, https://tc39.es/ecma262/#sec-properties-of-the-boolean-prototype-object
+// The Boolean prototype object is itself a Boolean object; it has a [[BooleanData]] internal slot with the value false.
+internal sealed class BooleanPrototype : BooleanObject 
+{
+    // The Boolean prototype object has a [[Prototype]] internal slot whose value is %Object.prototype%.
+    public BooleanPrototype(ObjectPrototype prototype) : base(prototype, false)
+    {
+    }
+}
