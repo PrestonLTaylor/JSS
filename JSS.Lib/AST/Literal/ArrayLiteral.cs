cee85a2fe48bc513e9a4c00c00456d7f12f28ff2
PrestonLTaylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-05-17 11:19:00 +0100

    JSS-75 Implement the defining of data elements for array literals
    We now successfully handle the data elements and elisions inside of an
    array literal.
    
    Example Code:
    ```js
    var a = [1,,];
    a.length; // Returns 2
    a[0]; // Returns 1
    a[1]; // Returns undefined
    a.hasOwnProperty(0); // Returns true
    a.hasOwnProperty(1); // Returns false
    ```

--- JSS.Lib/AST/Literal/ArrayLiteral.cs
+++ JSS.Lib/AST/Literal/ArrayLiteral.cs
@@ -12,12 +12,59 @@ internal sealed class ArrayLiteral : IExpression
     // 13.2.4.2 Runtime Semantics: Evaluation, https://tc39.es/ecma262/#sec-array-initializer-runtime-semantics-evaluation
     override public Completion Evaluate(VM vm)
     {
-        // FIXME: Implement the rest of the evaluation when we parse element lists
         // 1. Let array be ! ArrayCreate(0).
         var array = MUST(Array.ArrayCreate(vm, 0));
 
+        // 2. Perform ? ArrayAccumulation of ElementList with arguments array and 0.
+        var accumulationResult = ArrayAccumulation(vm, array);
+        if (accumulationResult.IsAbruptCompletion()) return accumulationResult;
+
         // 3. Return array.
         return array;
+    }
+
+    // 13.2.4.1 Runtime Semantics: ArrayAccumulation, https://tc39.es/ecma262/#sec-runtime-semantics-arrayaccumulation
+    private Completion ArrayAccumulation(VM vm, Array array)
+    {
+        // FIXME/NOTE: This doesn't follow the spec steps exactly, however, this should fully mimic the behaviour.
+        var nextIndex = 0;
+        foreach (var element in Elements)
+        {
+            // 1. If Elision is present, then
+            if (element is null)
+            {
+                // 1. Let len be nextIndex + 1.
+                var len = nextIndex + 1;
+
+                // 2. Perform ? Set(array, "length", 𝔽(len), true).
+                var setResult = Object.Set(vm, array, "length", len, true);
+                if (setResult.IsAbruptCompletion()) return setResult;
+
+                // 3. NOTE: The above step throws if len exceeds 2**32 - 1.
+
+                // 4. Return len.
+                // a. Set nextIndex to ? ArrayAccumulation of Elision with arguments array and nextIndex.
+                nextIndex = len;
+            }
+            else
+            {
+                // 2. Let initResult be ? Evaluation of AssignmentExpression.
+                var initResult = element.Evaluate(vm);
+                if (initResult.IsAbruptCompletion()) return initResult;
+
+                // 3. Let initValue be ? GetValue(initResult).
+                var initValue = initResult.Value.GetValue(vm);
+
+                // 4. Perform ! CreateDataPropertyOrThrow(array, ! ToString(𝔽(nextIndex)), initValue).
+                MUST(Object.CreateDataPropertyOrThrow(vm, array, nextIndex.ToString(), initValue.Value));
+
+                // 5. Return nextIndex + 1.
+                nextIndex++;
+            }
+        }
+
+        // Other steps return the index in the happy path, so we return nextIndex here.
+        return nextIndex;
     }
 
     public IReadOnlyList<IExpression?> Elements { get; }
