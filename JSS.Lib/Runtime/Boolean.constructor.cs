f9376fee891bffec1eed8d50eb692726065ac483
PrestonLTaylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-05-17 14:19:24 +0100

    JSS-81 Implement the Boolean constructor's call and construct methods

---
+++ JSS.Lib/Runtime/Boolean.constructor.cs
@@ -0,0 +1,36 @@
+﻿using JSS.Lib.AST.Values;
+using JSS.Lib.Execution;
+
+namespace JSS.Lib.Runtime;
+
+// 20.3.1 The Boolean Constructor, https://tc39.es/ecma262/#sec-boolean-constructor
+internal sealed class BooleanConstructor : Object, ICallable, IConstructable
+{
+    // The Boolean constructor has a [[Prototype]] internal slot whose value is %Function.prototype%.
+    public BooleanConstructor(FunctionPrototype prototype) : base(prototype)
+    {
+    }
+
+    // 20.3.1.1 Boolean ( value ), https://tc39.es/ecma262/#sec-boolean-constructor-boolean-value
+    public Completion Call(VM vm, Value thisArgument, List argumentList)
+    {
+        return Construct(vm, argumentList, Undefined.The);
+    }
+
+    // 20.3.1.1 Boolean ( value ), https://tc39.es/ecma262/#sec-boolean-constructor-boolean-value
+    public Completion Construct(VM vm, List argumentsList, Object newTarget)
+    {
+        // 1. Let b be ToBoolean(value).
+        var b = argumentsList[0].ToBoolean();
+
+        // 2. If NewTarget is undefined, return b.
+        if (newTarget.IsUndefined()) return b;
+
+        // FIXME: 3. Let O be ? OrdinaryCreateFromConstructor(NewTarget, "%Boolean.prototype%", « [[BooleanData]] »).
+        // 4. Set O.[[BooleanData]] to b.
+        var O = new BooleanObject(vm.ObjectPrototype, b);
+
+        // 5. Return O.
+        return O;
+    }
+}
