44d57f6498a6be2bfe42aa2b286303ef99e4f5d3
PrestonLTaylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-06-04 17:10:37 +0100

    JSS-108 Implement at for the String prototype
    ```js
    "abc".at(0); // Returns "a"
    "abc".at(-1); // Returns "c"
    
    var obj = new String("abc");
    obj.at(0); // Returns "a"
    obj.at(-1); // Returns "c"
    ```

--- JSS.Lib/Runtime/String.prototype.cs
+++ JSS.Lib/Runtime/String.prototype.cs
@@ -14,6 +14,9 @@ internal sealed class StringPrototype : Object
 
     public void Initialize(Realm realm, VM vm)
     {
+        // 22.1.3.1 String.prototype.at ( index ), https://tc39.es/ecma262/#sec-string.prototype.at
+        InternalDefineProperty(vm, "at", 1, at, new(true, false, true));
+
         // 22.1.3.6 String.prototype.constructor, The initial value of String.prototype.constructor is %String%.
         InternalDefineProperty("constructor", realm.StringConstructor, new(true, false, true));
 
@@ -25,6 +28,44 @@ internal sealed class StringPrototype : Object
 
         // 22.1.3.35 String.prototype.valueOf ( ), https://tc39.es/ecma262/#sec-string.prototype.valueof
         InternalDefineProperty(vm, "valueOf", 0, valueOf, new(true, false, true));
+    }
+
+    // 22.1.3.1 String.prototype.at ( index ), https://tc39.es/ecma262/#sec-string.prototype.at
+    private Completion at(VM vm, Value thisValue, List argumentList, Object newTarget)
+    {
+        // 1. Let O be ? RequireObjectCoercible(this value).
+        var O = thisValue.RequireObjectCoercible(vm);
+        if (O.IsAbruptCompletion()) return O;
+
+        // 2. Let S be ? ToString(O).
+        var S = O.Value.ToStringJS(vm);
+        if (S.IsAbruptCompletion()) return S.Completion;
+
+        // 3. Let len be the length of S.
+        var len = S.Value.Length;
+
+        // 4. Let relativeIndex be ? ToIntegerOrInfinity(index).
+        var relativeIndex = argumentList[0].ToIntegerOrInfinity(vm);
+        if (relativeIndex.IsAbruptCompletion()) return relativeIndex.Completion;
+
+        // 5. If relativeIndex ≥ 0, then
+        int k;
+        if (relativeIndex.Value >= 0)
+        {
+            k = (int)relativeIndex.Value;
+        }
+        // 6. Else,
+        else
+        {
+            // a. Let k be len + relativeIndex.
+            k = len + (int)relativeIndex.Value;
+        }
+
+        // 7. If k < 0 or k ≥ len, return undefined.
+        if (k < 0 || k >= len) return Undefined.The;
+
+        // 8. Return the substring of S from k to k + 1.
+        return S.Value.Substring(k, 1);
     }
 
     // 22.1.3.28 String.prototype.toLowerCase ( ), https://tc39.es/ecma262/multipage/text-processing.html#sec-string.prototype.tolowercase
